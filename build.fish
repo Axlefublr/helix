0a146d0bf36af5f9872f90d08a63ec5529ca2fb9
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-10-20 21:52:07 +0800

    build: script runs, rather than installs

--- build.fish
+++ build.fish
@@ -4,5 +4,5 @@ cargo +nightly fmt || return 1
 cargo clippy || return 1
 cargo test || return 1
 cargo xtask docgen || return 1
-cargo install --path helix-term --locked || return 1
+cargo run || return 1
 bell
