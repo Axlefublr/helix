0ecd67ec2c948e81b9b8c87b31661a05afd50044
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-12-08 02:47:03 +0800

    `:s` alias for `:run-shell-command`

--- helix-term/src/commands/typed.rs
+++ helix-term/src/commands/typed.rs
@@ -3102,7 +3102,7 @@ pub const TYPABLE_COMMAND_LIST: &[TypableCommand] = &[
     },
     TypableCommand {
         name: "run-shell-command",
-        aliases: &["sh"],
+        aliases: &["sh", "s"],
         doc: "Run a shell command",
         fun: run_shell_command,
         signature: CommandSignature::all(completers::filename)
