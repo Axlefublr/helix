a62f7805b686bf0a2286f076ea291fff50664f9f
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-03-11 16:10:51 +0800

    expansions

--- helix-view/src/expansion.rs
+++ helix-view/src/expansion.rs
@@ -41,6 +41,10 @@ pub enum Variable {
     SelectionLineStart,
     // The one-indexed line number of the end of the primary selection in the currently focused document.
     SelectionLineEnd,
+    FullPath,
+    WorkingDirectory,
+    RelativePath,
+    BufferParent,
 }
 
 impl Variable {
@@ -53,6 +57,10 @@ impl Variable {
         Self::Selection,
         Self::SelectionLineStart,
         Self::SelectionLineEnd,
+        Self::FullPath,
+        Self::WorkingDirectory,
+        Self::RelativePath,
+        Self::BufferParent,
     ];
 
     pub const fn as_str(&self) -> &'static str {
@@ -65,6 +73,10 @@ impl Variable {
             Self::Selection => "selection",
             Self::SelectionLineStart => "selection_line_start",
             Self::SelectionLineEnd => "selection_line_end",
+            Self::FullPath => "full_path",
+            Self::WorkingDirectory => "working_directory",
+            Self::RelativePath => "relative_path",
+            Self::BufferParent => "buffer_parent",
         }
     }
 
@@ -78,6 +90,10 @@ impl Variable {
             "selection" => Some(Self::Selection),
             "selection_line_start" => Some(Self::SelectionLineStart),
             "selection_line_end" => Some(Self::SelectionLineEnd),
+            "full_path" => Some(Self::FullPath),
+            "working_directory" => Some(Self::WorkingDirectory),
+            "relative_path" => Some(Self::RelativePath),
+            "buffer_parent" => Some(Self::BufferParent),
             _ => None,
         }
     }
@@ -250,5 +266,34 @@ fn expand_variable(editor: &Editor, variable: Variable) -> Result<Cow<'static, s
             let end_line = doc.selection(view.id).primary().line_range(text).1;
             Ok(Cow::Owned((end_line + 1).to_string()))
         }
+        Variable::FullPath => Ok(if let Some(path) = doc.path() {
+            Cow::Owned(helix_stdx::path::fold_home_dir(path).display().to_string())
+        } else {
+            Cow::Borrowed("")
+        }),
+        Variable::WorkingDirectory => Ok(Cow::Owned(
+            helix_stdx::path::fold_home_dir(helix_stdx::env::current_working_dir())
+                .display()
+                .to_string(),
+        )),
+        Variable::RelativePath => Ok(
+            if let Some(path) = doc.path().map(|the| {
+                helix_stdx::path::get_relative_path(the)
+                    .into_owned()
+                    .display()
+                    .to_string()
+            }) {
+                Cow::Owned(path)
+            } else {
+                Cow::Borrowed("")
+            },
+        ),
+        Variable::BufferParent => Ok(if let Some(path) = doc.path() {
+            let mut path = path.clone();
+            path.pop();
+            Cow::Owned(path.display().to_string())
+        } else {
+            Cow::Borrowed("")
+        }),
     }
 }
