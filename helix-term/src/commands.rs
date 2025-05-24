5f1b0404d20b89e18f9327080b51db021fda90a9
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-05-25 06:33:05 +0800

    option: whichkey

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -6080,7 +6080,9 @@ fn select_textobject(cx: &mut Context, objtype: textobject::TextObject) {
         (" ", "... or any character acting as a pair"),
     ];
 
-    cx.editor.autoinfo = Some(Info::new(title, &help_text));
+    if cx.editor.config().whichkey {
+        cx.editor.autoinfo = Some(Info::new(title, &help_text));
+    }
 }
 
 static SURROUND_HELP_TEXT: [(&str, &str); 6] = [
@@ -6137,10 +6139,12 @@ fn surround_add(cx: &mut Context) {
         exit_select_mode(cx);
     });
 
-    cx.editor.autoinfo = Some(Info::new(
-        "Surround selections with",
-        &SURROUND_HELP_TEXT[1..],
-    ));
+    if cx.editor.config().whichkey {
+        cx.editor.autoinfo = Some(Info::new(
+            "Surround selections with",
+            &SURROUND_HELP_TEXT[1..],
+        ));
+    }
 }
 
 fn surround_replace(cx: &mut Context) {
@@ -6202,16 +6206,20 @@ fn surround_replace(cx: &mut Context) {
             exit_select_mode(cx);
         });
 
-        cx.editor.autoinfo = Some(Info::new(
-            "Replace with a pair of",
-            &SURROUND_HELP_TEXT[1..],
-        ));
+        if cx.editor.config().whichkey {
+            cx.editor.autoinfo = Some(Info::new(
+                "Replace with a pair of",
+                &SURROUND_HELP_TEXT[1..],
+            ));
+        }
     });
 
-    cx.editor.autoinfo = Some(Info::new(
-        "Replace surrounding pair of",
-        &SURROUND_HELP_TEXT,
-    ));
+    if cx.editor.config().whichkey {
+        cx.editor.autoinfo = Some(Info::new(
+            "Replace surrounding pair of",
+            &SURROUND_HELP_TEXT,
+        ));
+    }
 }
 
 fn surround_delete(cx: &mut Context) {
@@ -6242,7 +6250,9 @@ fn surround_delete(cx: &mut Context) {
         exit_select_mode(cx);
     });
 
-    cx.editor.autoinfo = Some(Info::new("Delete surrounding pair of", &SURROUND_HELP_TEXT));
+    if cx.editor.config().whichkey {
+        cx.editor.autoinfo = Some(Info::new("Delete surrounding pair of", &SURROUND_HELP_TEXT));
+    }
 }
 
 #[derive(Eq, PartialEq)]
