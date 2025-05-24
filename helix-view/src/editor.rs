d692fc091c38ce840c241388bb33ca0dc6c228ef
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-05-25 06:33:36 +0800

    option: ephemeral-messages

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -385,6 +385,8 @@ pub struct Config {
     pub whichkey: bool,
     /// Whether to display the statusline at all. Defaults to true.
     pub should_statusline: bool,
+    /// Whether to make messages take up an entire line in the UI, or appear over editor text. Defaults to false (take up entire line in the UI).
+    pub ephemeral_messages: bool,
     /// Disable the `.` mapping that repeats your last action, making `.` rebindable. Defaults to false.
     pub disable_dot_repeat: bool,
     pub show_diagnostics: bool,
@@ -1070,6 +1072,7 @@ impl Default for Config {
             rainbow_brackets: false,
             should_statusline: true,
             whichkey: true,
+            ephemeral_messages: false,
             disable_dot_repeat: false,
             show_diagnostics: true,
         }
