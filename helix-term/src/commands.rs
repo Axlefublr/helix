2e1c8b8fc621639fcc97f85ed59651244d56bcae
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-05-26 09:29:50 +0800

    action: harp

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -1,4 +1,5 @@
 mod axlefublr_count_selections;
+mod axlefublr_harp;
 mod axlefublr_surround_add_tag;
 mod axlefublr_toggle_line_select;
 pub(crate) mod dap;
@@ -6,6 +7,7 @@ pub(crate) mod lsp;
 pub(crate) mod typed;
 
 use axlefublr_count_selections::*;
+use axlefublr_harp::*;
 use axlefublr_surround_add_tag::*;
 use axlefublr_toggle_line_select::*;
 pub use dap::*;
@@ -612,6 +614,13 @@ impl MappableCommand {
         count_selections, "Print amount of selections to messages",
         toggle_line_select, "Toggle between trim_selections and extend_to_line_bounds",
         surround_add_tag, "Surround selections with an html tag",
+        harp_file, "Interact with a file harp",
+        harp_relative_file, "Interact with a relative file harp",
+        harp_cwd, "Interact with a cwd harp",
+        harp_search, "Interact with a search harp",
+        harp_register, "Interact with a register harp",
+        harp_command, "Interact with a command harp",
+        harp_mark, "Interact with a mark harp",
     );
 }
 
