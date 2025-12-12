8bfa3dfdd881dc5d56e275ba7e642de02f9210e8
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-12-12 08:04:14 +0800

    search wrap is silent

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -2214,7 +2214,7 @@ fn search_impl(
     // If we didn't find a match before, lets wrap the search.
     if mat.is_none() {
         if show_warnings {
-            editor.set_status("Wrapped around document");
+            // editor.set_status("Wrapped around document");
         }
 
         let doc = doc!(editor).text().slice(..);
